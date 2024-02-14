2d200c3b652b5ece38657f924d1479af7a660234
gadgelogger <sarayou273142910@gmail.com>
2024-02-15 02:43:09 +0900

    fix:静的解析(import_sorte)にて指摘された箇所を修正

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:kokorahenn_flutter/model/shop.dart'; // モデルのインポートパスを適切に設定してください
+import 'package:kokorahenn_flutter/model/shop.dart';
 
 class ShopService {
   final Uri url = Uri.parse(
