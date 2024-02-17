f71af7934a3a2a08fbd3bacb98c6bebdd567dc5d
gadgelogger <sarayou273142910@gmail.com>
2024-02-18 02:56:31 +0900

    add:UIのみを作成。

--- lib/screens/setting.dart
+++ lib/screens/setting.dart
@@ -1,5 +1,6 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
@@ -10,8 +11,64 @@ class SettingPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('設定'),
       ),
-      body: const Center(
-        child: Text('設定画面'),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: const Text('配色'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.color_lens),
+                title: const Text('テーマカラー'),
+                value: const Text('端末の設定に従う'),
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: const Text('その他'),
+            tiles: <SettingsTile>[
+              SettingsTile(
+                title: const Text('このアプリを他の人に勧める'),
+                leading: const Icon(Icons.share),
+                onPressed: (BuildContext context) {
+                  // ここに遷移先の画面を指定
+                },
+              ),
+              SettingsTile(
+                title: const Text('プライバシーポリシー'),
+                leading: const Icon(Icons.privacy_tip),
+                onPressed: (BuildContext context) {
+                  // ここに遷移先の画面を指定
+                },
+              ),
+              SettingsTile(
+                title: const Text('お問い合わせはこちら'),
+                leading: const Icon(Icons.info),
+                onPressed: (BuildContext context) {
+                  // ここに遷移先の画面を指定
+                },
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: const Text('このアプリについて'),
+            tiles: <SettingsTile>[
+              SettingsTile(
+                title: const Text('ライセンス'),
+                leading: const Icon(Icons.terminal),
+                onPressed: (BuildContext context) {
+                  // ここに遷移先の画面を指定},
+                },
+              ),
+              SettingsTile(
+                title: const Text('バージョン情報'),
+                leading: const Icon(Icons.privacy_tip),
+                onPressed: (BuildContext context) {
+                  // ここに遷移先の画面を指定
+                },
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
