7edf036996011c05e59c4fceba7ae0f488741a07
gadgelogger <sarayou273142910@gmail.com>
2024-02-18 01:10:08 +0900

    add:geolocatorを使用して緯度経度を取得し、APIへリクエストを送る処理を実装しました。

---
+++ lib/api/service/location_service.dart
@@ -0,0 +1,21 @@
+// location_service.dart
+import 'package:geolocator/geolocator.dart';
+
+class LocationService {
+  Future<Position> getCurrentPosition() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception('Location permissions are permanently denied');
+    }
+
+    return Geolocator.getCurrentPosition();
+  }
+}
