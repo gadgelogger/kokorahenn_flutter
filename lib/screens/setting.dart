885d7849d5f32f9c37d8fb4741f349676c0b914d
gadgelogger <sarayou273142910@gmail.com>
2024-02-20 03:10:34 +0900

    add:flutter_riverpodの追加とテーマの変更

--- lib/screens/setting.dart
+++ lib/screens/setting.dart
@@ -3,58 +3,79 @@ import 'dart:io';
 
 // Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Package imports:
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher_string.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class SettingPage extends StatefulWidget {
+import '../theme/theme_mode_provider.dart';
+
+class SettingPage extends ConsumerWidget {
   const SettingPage({super.key});
 
   @override
-  State<SettingPage> createState() => _SettingPageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final toggle = ref.read(themeModeProvider.notifier).toggle;
 
-class _SettingPageState extends State<SettingPage> {
-  late Future<PackageInfo> _packageInfoFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    _packageInfoFuture = PackageInfo.fromPlatform();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('設定'),
       ),
       body: FutureBuilder<PackageInfo>(
-        future: _packageInfoFuture,
+        future: PackageInfo.fromPlatform(),
         builder: (context, snapshot) {
-          final packageInfo = snapshot.data ??
-              PackageInfo(
-                appName: 'Unknown',
-                packageName: 'Unknown',
-                version: 'Unknown',
-                buildNumber: 'Unknown',
-                buildSignature: 'Unknown',
-                installerStore: 'Unknown',
-              );
-
+          final packageInfo = snapshot.data;
           return SettingsList(
             sections: [
               SettingsSection(
                 title: const Text('配色'),
                 tiles: <SettingsTile>[
                   SettingsTile.navigation(
+                    title: const Text('テーマ'),
                     leading: const Icon(Icons.color_lens),
-                    title: const Text('テーマカラー'),
-                    value: const Text('端末の設定に従う'),
-                  ),
+                    trailing: Text(
+                      {
+                            ThemeMode.light: 'ライトモード',
+                            ThemeMode.dark: 'ダークモード',
+                            ThemeMode.system: 'システム設定に従う',
+                          }[ref.watch(themeModeProvider)] ??
+                          '不明',
+                    ),
+                    onPressed: (_) async {
+                      final ThemeMode? selectedThemeMode =
+                          await showDialog<ThemeMode>(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return SimpleDialog(
+                            title: const Text('テーマを選択'),
+                            children: <Widget>[
+                              SimpleDialogOption(
+                                onPressed: () =>
+                                    Navigator.pop(context, ThemeMode.system),
+                                child: const Text('システム設定に従う'),
+                              ),
+                              SimpleDialogOption(
+                                onPressed: () =>
+                                    Navigator.pop(context, ThemeMode.light),
+                                child: const Text('ライトモード'),
+                              ),
+                              SimpleDialogOption(
+                                onPressed: () =>
+                                    Navigator.pop(context, ThemeMode.dark),
+                                child: const Text('ダークモード'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                      if (selectedThemeMode != null) {
+                        await toggle();
+                      }
+                    },
+                  )
                 ],
               ),
               SettingsSection(
@@ -92,18 +113,16 @@ class _SettingPageState extends State<SettingPage> {
                   SettingsTile.navigation(
                     title: const Text('ライセンス'),
                     leading: const Icon(Icons.terminal),
-                    onPressed: (_) {
-                      showLicensePage(
-                        context: context,
-                        applicationName: packageInfo.appName,
-                        applicationVersion: packageInfo.version,
-                      );
-                    },
+                    onPressed: (_) => showLicensePage(
+                      context: context,
+                      applicationName: packageInfo?.appName ?? '不明',
+                      applicationVersion: packageInfo?.version ?? '不明',
+                    ),
                   ),
                   SettingsTile(
                     title: const Text('バージョン情報'),
-                    leading: const Icon(Icons.privacy_tip),
-                    value: Text(packageInfo.version),
+                    leading: const Icon(Icons.info_outline),
+                    value: Text(packageInfo?.version ?? '不明'),
                   ),
                 ],
               ),
---
+++ lib/theme/theme_mode_provider.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kokorahenn_flutter/theme/share_preferences_instance.dart';
+
+class ThemeModeNotifier extends StateNotifier<ThemeMode> {
+  ThemeModeNotifier() : super(ThemeMode.system) {
+    state = _loadThemeMode() ?? ThemeMode.system;
+  }
+  static const String keyThemeMode = 'theme_mode';
+
+  final _prefs = SharedPreferencesInstance().prefs;
+
+  Future<void> toggle() async {
+    ThemeMode themeMode;
+    switch (state) {
+      case ThemeMode.light:
+        themeMode = ThemeMode.dark;
+      case ThemeMode.dark:
+        themeMode = ThemeMode.system;
+      case ThemeMode.system:
+        themeMode = ThemeMode.light;
+    }
+    await _saveThemeMode(themeMode).then((value) {
+      if (value) {
+        state = themeMode;
+      }
+    });
+  }
+
+  ThemeMode? _loadThemeMode() {
+    final loaded = _prefs.getString(keyThemeMode);
+    if (loaded == null) {
+      return null;
+    }
+    return ThemeMode.values.byName(loaded);
+  }
+
+  Future<bool> _saveThemeMode(ThemeMode themeMode) =>
+      _prefs.setString(keyThemeMode, themeMode.name);
+}
+
+final themeModeProvider = StateNotifierProvider<ThemeModeNotifier, ThemeMode>(
+  (ref) => ThemeModeNotifier(),
+);
