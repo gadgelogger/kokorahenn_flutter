b97f7767f361fffb5adcd0890dcd709b8dbf4fb5
gadgelogger <sarayou273142910@gmail.com>
2024-02-20 01:31:06 +0900

    add:search_detail_screen.dartのWidgetの分割を行いました。

--- lib/screens/search_detail_screen.dart
+++ lib/screens/search_detail_screen.dart
@@ -1,8 +1,11 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:kokorahenn_flutter/widgets/detail/detail_buttons.dart';
+import 'package:kokorahenn_flutter/widgets/detail/detail_info.dart';
 
 // Project imports:
 import '../model/dto/shop.dart';
+import '../widgets/detail/detail_image.dart';
 
 class SearchDetailPage extends StatelessWidget {
   const SearchDetailPage({super.key, required this.shop});
@@ -11,165 +14,97 @@ class SearchDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(shop.name ?? '詳細'),
-      ),
+      appBar: AppBar(title: Text(shop.name ?? '詳細')),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child: Image.network(
-                shop.photo?.pc?.l ?? '',
-                width: 200,
-                height: 200,
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) {
-                  return Container(
-                    width: 200,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(100),
-                    ),
-                    child: Icon(
-                      Icons.broken_image,
-                      size: 100,
-                      color: Colors.grey[500],
-                    ),
-                  );
-                },
-              ),
-            ),
+            DetailImage(imageUrl: shop.photo?.pc?.l ?? ''),
             const SizedBox(height: 20),
             const Divider(),
             const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _buildActionButton(Icons.map_outlined),
-                _buildActionButton(Icons.travel_explore_outlined),
-                _buildActionButton(Icons.search_outlined),
-              ],
-            ),
+            const DetailButtons(),
             const SizedBox(height: 20),
             const Divider(),
-            _buildInfoRow(
-              Icons.map_outlined,
-              '住所:',
-              shop.address ?? '住所不明',
+            DetailInfo(
+              icon: Icons.map_outlined,
+              info: '住所:',
+              text: shop.address ?? '住所不明',
             ),
-            _buildInfoRow(
-              Icons.access_time_outlined,
-              '営業時間:',
-              shop.open ?? '営業時間不明',
+            DetailInfo(
+              icon: Icons.access_time_outlined,
+              info: '営業時間:',
+              text: shop.open ?? '営業時間不明',
             ),
-            _buildInfoRow(
-              Icons.block,
-              '閉業日:',
-              shop.close ?? '閉業日不明',
+            DetailInfo(
+              icon: Icons.block_outlined,
+              info: '閉業日:',
+              text: shop.close ?? '閉業日不明',
             ),
-            _buildInfoRow(
-              Icons.category_outlined,
-              'ジャンル',
-              shop.genre?.name ?? 'ジャンル不明',
+            DetailInfo(
+              icon: Icons.category_outlined,
+              info: 'ジャンル:',
+              text: shop.genre?.name ?? 'ジャンル不明',
             ),
-            _buildInfoRow(
-              Icons.restaurant_outlined,
-              '食べ放題:',
-              shop.freeFood ?? '食べ放題不明',
+            DetailInfo(
+              icon: Icons.restaurant_rounded,
+              info: '食べ放題:',
+              text: shop.freeFood ?? '食べ放題不明',
             ),
-            _buildInfoRow(
-              Icons.sports_bar_outlined,
-              '飲み放題:',
-              shop.freeDrink ?? '飲み放題不明',
+            DetailInfo(
+              icon: Icons.sports_bar_outlined,
+              info: '飲み放題:',
+              text: shop.freeDrink ?? '飲み放題不明',
             ),
-            _buildInfoRow(
-              Icons.air_outlined,
-              'コース:',
-              shop.course ?? 'コース不明',
+            DetailInfo(
+              icon: Icons.air_outlined,
+              info: 'コース:',
+              text: shop.course ?? 'コース不明',
             ),
-            _buildInfoRow(
-              Icons.no_meeting_room_outlined,
-              '総席数:',
-              shop.capacity.toString(),
+            DetailInfo(
+              icon: Icons.chair_alt_outlined,
+              info: '総席数:',
+              text: shop.capacity.toString(),
             ),
-            _buildInfoRow(
-              Icons.smoking_rooms_outlined,
-              '禁煙:',
-              shop.nonSmoking ?? '禁煙不明',
+            DetailInfo(
+              icon: Icons.smoking_rooms_outlined,
+              info: '禁煙:',
+              text: shop.nonSmoking ?? '禁煙不明',
             ),
-            _buildInfoRow(
-              Icons.payments_outlined,
-              '平均予算:',
-              shop.budget?.average ?? '平均予算不明',
+            DetailInfo(
+              icon: Icons.payments_outlined,
+              info: '平均予算:',
+              text: shop.budget?.average ?? '平均予算不明',
             ),
-            _buildInfoRow(
-              Icons.payment_outlined,
-              'カード決済:',
-              shop.card ?? 'カード決済不明',
+            DetailInfo(
+              icon: Icons.payment_outlined,
+              info: 'カード決済:',
+              text: shop.card ?? 'カード決済不明',
             ),
-            _buildInfoRow(
-              Icons.no_crash_outlined,
-              '駐車場:',
-              shop.parking ?? '駐車場不明',
+            DetailInfo(
+              icon: Icons.no_crash_outlined,
+              info: '駐車場:',
+              text: shop.parking ?? '駐車場不明',
             ),
-            _buildInfoRow(
-              Icons.wheelchair_pickup,
-              'バリアフリー:',
-              shop.barrierFree ?? 'バリアフリー不明',
+            DetailInfo(
+              icon: Icons.wheelchair_pickup_outlined,
+              info: 'バリアフリー:',
+              text: shop.barrierFree ?? 'バリアフリー不明',
             ),
-            _buildInfoRow(
-              Icons.access_time_outlined,
-              'その他設備:',
-              shop.otherMemo ?? 'その他設備不明',
+            DetailInfo(
+              icon: Icons.house_outlined,
+              info: 'その他設備:',
+              text: shop.otherMemo ?? 'その他設備不明',
             ),
-            _buildInfoRow(
-              Icons.block,
-              '備考:',
-              shop.shopDetailMemo ?? '備考不明',
+            DetailInfo(
+              icon: Icons.note_outlined,
+              info: '備考:',
+              text: shop.shopDetailMemo ?? '備考不明',
             ),
           ],
         ),
       ),
-    );
-  }
-
-//アクションボタンを表示するためのウィジェット
-  Widget _buildActionButton(IconData icon) {
-    return IconButton(
-      icon: Icon(icon),
-      style: ElevatedButton.styleFrom(
-        shape: const CircleBorder(),
-        padding: const EdgeInsets.all(20),
-      ),
-      onPressed: () {},
-    );
-  }
-
-//住所や営業時間を表示するためのウィジェット
-  Widget _buildInfoRow(IconData icon, String info, String text) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: Row(
-            children: [
-              Icon(icon),
-              Text(info),
-              Flexible(
-                child: Text(
-                  text,
-                  overflow: TextOverflow.visible,
-                ),
-              ),
-            ],
-          ),
-        ),
-        const Divider(),
-      ],
     );
   }
 }
---
+++ lib/widgets/detail/detail_buttons.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class DetailButtons extends StatelessWidget {
+  const DetailButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        IconButton.filledTonal(
+          icon: const Icon(Icons.map_outlined),
+          padding: const EdgeInsets.all(20),
+          onPressed: () {},
+        ),
+        IconButton.filledTonal(
+          icon: const Icon(Icons.travel_explore_outlined),
+          padding: const EdgeInsets.all(20),
+          onPressed: () {},
+        ),
+        IconButton.filledTonal(
+          icon: const Icon(Icons.search_outlined),
+          padding: const EdgeInsets.all(20),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
