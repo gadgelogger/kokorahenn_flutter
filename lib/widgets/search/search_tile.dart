a9dcc55b1a4e0f37d46bd5e9813b1ff1b503e839
gadgelogger <sarayou273142910@gmail.com>
2024-02-21 01:05:24 +0900

    fix:Add expansion_tile_card package and update UI in SearchTile

--- lib/widgets/search/search_tile.dart
+++ lib/widgets/search/search_tile.dart
@@ -1,4 +1,5 @@
 // Flutter imports:
+import 'package:expansion_tile_card/expansion_tile_card.dart';
 import 'package:flutter/material.dart';
 
 // Project imports:
@@ -15,28 +16,92 @@ class SearchTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: ClipRRect(
-        borderRadius: BorderRadius.circular(100),
-        child: Image.network(
-          shop.logoImage ?? '',
-          width: 50,
-          height: 50,
-          fit: BoxFit.cover,
-          errorBuilder: (context, error, stackTrace) => const Icon(Icons.error),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+      child: ExpansionTileCard(
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(100),
+          child: Image.network(
+            shop.logoImage ?? '',
+            width: 50,
+            height: 50,
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) =>
+                const Icon(Icons.error),
+          ),
         ),
+        title: Text(
+          shop.name ?? '名前なし',
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+        subtitle: Text(
+          shop.address ?? '住所不明',
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+        children: <Widget>[
+          const Divider(
+            thickness: 1.0,
+            height: 1.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const Icon(Icons.access_time_outlined),
+                    const SizedBox(width: 8),
+                    Flexible(
+                      child: Text(
+                        shop.open ?? '営業時間不明',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    const Icon(Icons.block_outlined),
+                    const SizedBox(width: 8),
+                    Flexible(
+                      child: Text(
+                        shop.close ?? '閉業日不明',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    const Icon(Icons.category_outlined),
+                    const SizedBox(width: 8),
+                    Flexible(
+                      child: Text(
+                        shop.genre?.name ?? 'ジャンル不明',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                        onPressed: onTap, child: const Text('お店の詳細を見る')),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      title: Text(
-        shop.name ?? '名前なし',
-        maxLines: 2,
-        overflow: TextOverflow.ellipsis,
-      ),
-      subtitle: Text(
-        shop.address ?? '住所不明',
-        maxLines: 2,
-        overflow: TextOverflow.ellipsis,
-      ),
-      onTap: onTap,
     );
   }
 }
