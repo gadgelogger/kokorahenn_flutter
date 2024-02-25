574003f1d427dd329ea4a4d6507bc80ac9f9690e
gadgelogger <sarayou273142910@gmail.com>
2024-02-26 03:00:57 +0900

    fix:APIキーの処理を変更

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,10 +2,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kokorahenn_flutter/api/service/location_notifier.dart';
+import 'package:kokorahenn_flutter/i18n/strings.g.dart';
 import 'package:kokorahenn_flutter/screens/search_screen.dart';
 // Project imports:
 import 'package:kokorahenn_flutter/screens/setting_screen.dart';
-import 'package:kokorahenn_flutter/widgets/bottom_navigation.dart';
 
 final baseTabViewProvider = StateProvider<ViewType>((ref) => ViewType.home);
 
@@ -23,15 +23,24 @@ class MainPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     ref.read(locationNotifierProvider).fetchCurrentUserPosition();
     final view = ref.watch(baseTabViewProvider.state);
-
+    final navigationLabel = t.bottomNavigation;
     return Scaffold(
       body: Center(
         child: widgets[view.state.index],
       ),
-      bottomNavigationBar: BottomNavigation(
+      bottomNavigationBar: BottomNavigationBar(
         currentIndex: view.state.index,
-        onItemSelected: (int index) =>
-            view.update((state) => ViewType.values[index]),
+        onTap: (int index) => view.update((state) => ViewType.values[index]),
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.home),
+            label: navigationLabel.home,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.settings),
+            label: navigationLabel.setting,
+          ),
+        ],
       ),
     );
   }
