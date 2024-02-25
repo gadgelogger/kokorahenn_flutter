bc59dfc3e4241d5bbb8bc4599afa52322cda5d5a
gadgelogger <sarayou273142910@gmail.com>
2024-02-26 03:19:26 +0900

    main_screen.dartにて静的解析で指摘された部分を修正

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,10 +1,8 @@
-// Flutter imports:
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kokorahenn_flutter/api/service/location_notifier.dart';
 import 'package:kokorahenn_flutter/i18n/strings.g.dart';
 import 'package:kokorahenn_flutter/screens/search_screen.dart';
-// Project imports:
 import 'package:kokorahenn_flutter/screens/setting_screen.dart';
 
 final baseTabViewProvider = StateProvider<ViewType>((ref) => ViewType.home);
@@ -22,15 +20,17 @@ class MainPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     ref.read(locationNotifierProvider).fetchCurrentUserPosition();
-    final view = ref.watch(baseTabViewProvider.state);
+    final viewState = ref.watch(baseTabViewProvider);
     final navigationLabel = t.bottomNavigation;
     return Scaffold(
       body: Center(
-        child: widgets[view.state.index],
+        child: widgets[viewState.index],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: view.state.index,
-        onTap: (int index) => view.update((state) => ViewType.values[index]),
+        currentIndex: viewState.index,
+        onTap: (int index) {
+          ref.read(baseTabViewProvider.notifier).state = ViewType.values[index];
+        },
         items: [
           BottomNavigationBarItem(
             icon: const Icon(Icons.home),
